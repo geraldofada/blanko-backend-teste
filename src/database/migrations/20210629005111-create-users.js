226d1ca7c07116bfb7817ccd77e43178780eaac1
Geraldo Fada <geraldofada.neto@gmail.com>
2021-06-28 23:14:20 -0300

    email is unique

--- src/database/migrations/20210629005111-create-users.js
+++ src/database/migrations/20210629005111-create-users.js
@@ -1,6 +1,5 @@
 'use-stritc';
 
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
      await queryInterface.createTable('users', {
---
+++ src/database/migrations/20210629015918-users-email-unique.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.changeColumn('users', 'email', {
+       type: Sequelize.STRING,
+       allowNull: false,
+       unique: true,
+     });
+  },
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.changeColumn('users', 'email', {
+       type: Sequelize.STRING,
+       allowNull: false,
+       unique: false,
+     });
+  }
+};
