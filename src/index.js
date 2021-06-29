ca3d1d0509f29dcc3a8ce19549be9c6cbec3b342
Geraldo Fada <geraldofada.neto@gmail.com>
2021-06-29 16:23:59 -0300

    cors configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const users = require('./routes/users');
 
+const corsOptions = {
+  origin: process.env.ORIGIN_FRONTEND,
+  optionsSuccessStatus: 200
+};
+
 app = express();
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/users', users);
 
